294ced3ae038f563358404e8c47df0e8e30d8da9
mylhb9 <mylhb9@naver.com>
2021-11-14 20:04:38 +0900

    싱글톤 까지 공부

---
+++ src/main/java/sec10/exam02_static_block/TelevisionExample.java
@@ -0,0 +1,10 @@
+package sec10.exam02_static_block;
+
+public class TelevisionExample {
+    public static void main(String[] args) {
+        System.out.println(Telelvision.info);
+        System.out.println(Telelvision.from1To10Sum);
+    }
+
+
+}
